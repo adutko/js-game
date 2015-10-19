6430dab1abda9032eb17babd6a98a51c5b906337
Allyson Dutko <allyson.dutko@instrument.com>
2015-10-19 13:52:30 -0700

    Stubbing out initial project, experimenting with Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
+
+var paths = {
+  sassSrcPath: ['./source/css/*.scss'],
+  sassDestPath: ['./build/css'] // var does not work below
+};
+
+
+gulp.task('styles', function () {
+  return sass(paths.sassSrcPath, {})
+  .pipe(autoprefixer('last 2 version'))
+  //.pipe(gulp.dest(paths.sassDestPath));
+  .pipe(gulp.dest('./build/css'))
+  .pipe(rename({suffix: '.min'}))
+  .pipe(minifycss())
+  .pipe(gulp.dest('./build/css'));
+});
+
+
+gulp.task('process-scripts', function () {
+  return gulp.src('source/js/*js')
+})
+
+
+gulp.task('default', function () {
+  console.log('I have configured a gulpfile');
+});
